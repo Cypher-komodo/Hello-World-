273796d0742f925d1a4ded76a10448f0cc62c79c
MANSOURDAHBI <code00net@gmail.com>
2024-11-21 23:46:05 +0100

    Add Preface

--- Hello/main.c
+++ Hello/main.c
@@ -1,3 +1,12 @@
+// main.c 
+
+/*****************************************************************************************
+* include information about standard Library define a function called main
+* that recevived no argument values statement of main are enclosed in 
+* braces main calls library function printf to print this sequnec of characters
+* \n represents the newline character  
+******************************************************************************************/ 
+
 #include <stdio.h>
 
 int main(){
